eb6934b8fb08a5882036b252e550752fe7eafedd
Elena Rivilla <mariaelenarivilla@paucasesnovescifp.cat>
2025-04-15 21:48:03 +0200

    Refactor app component and routing; add environment configuration and Tailwind CSS setup

---
+++ PlanGo_frontend/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyBCc0wEGdTx2WhvmoIyxjeKtifeb0hRGdc",
+      authDomain: "fct24-25.firebaseapp.com",
+      projectId: "fct24-25",
+      storageBucket: "fct24-25.firebasestorage.app",
+      messagingSenderId: "807374900325",
+      appId: "1:807374900325:web:ea484962625a86afb64a8c",
+      measurementId: "G-1JJCTJSH3L"
+    }
+  };
+  
