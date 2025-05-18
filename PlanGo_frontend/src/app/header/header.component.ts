8c9b2f6cf091103662ccc9af7544ff2cf5569662
Elena Rivilla <mariaelenarivilla@paucasesnovescifp.cat>
2025-05-19 00:13:15 +0200

    style: Enhance button hover effects in header component with animations

--- PlanGo_frontend/src/app/header/header.component.ts
+++ PlanGo_frontend/src/app/header/header.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  animations: [
+    trigger('hoverAnimation', [
+      state('default', style({ transform: 'scale(1)' })),
+      state('hover', style({ transform: 'scale(1.2)' })),
+      transition('default <=> hover', animate('300ms ease-in-out')),
+    ]),
+  ],
 })
 export class HeaderComponent {
+  buttonStates: { [key: string]: string } = {
+    itineraries: 'default',
+    expenses: 'default',
+    savedPlaces: 'default',
+    profile: 'default',
+  };
+
   constructor( private router: Router){}
+
+  setHoverState(button: string, state: string): void {
+    this.buttonStates[button] = state;
+  }
 
   goToExpense(){
     this.router.navigate(["/expenses"]);
