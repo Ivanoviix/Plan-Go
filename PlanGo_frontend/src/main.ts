fdba21d344e6983f77709917f07d71ec9167ed09
Elena Rivilla <mariaelenarivilla@paucasesnovescifp.cat>
2025-06-06 22:04:30 +0200

    refactor: Remove unnecessary Firebase auth imports and clean up code

--- PlanGo_frontend/src/main.ts
+++ PlanGo_frontend/src/main.ts
@@ -2,7 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { appConfig } from './app/app.config';
-import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth';
 
 bootstrapApplication(AppComponent, {
   providers: [
