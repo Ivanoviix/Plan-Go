69fb8a1c557d9c5124bc79a5982e74f5b0b6509e
Elena Rivilla <mariaelenarivilla@paucasesnovescifp.cat>
2025-06-07 18:52:33 +0200

    feat: Enhance map component with photo carousel and integrate Google Places API key retrieval

--- PlanGo_frontend/src/app/map/map.component.ts
+++ PlanGo_frontend/src/app/map/map.component.ts
@@ -3,6 +3,7 @@ import { GoogleMapsModule } from '@angular/google-maps';
 import { CommonModule } from '@angular/common';
 import { ViewChild } from '@angular/core';
 import { MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { ApiKeyService } from '../core/services/api-key.service';
 
 @Component({
   standalone: true,
@@ -24,21 +25,54 @@ export class MapComponent implements OnInit {
   @Input() markers: { lat: number, lng: number, label?: string, place?: any }[] = [];
   @Input() selectedPlace: any = null;
   activeMarker: any = null;
+  activePhotoIndex: number = 0;
+  selectedPlaceImages: any[] = [];
+  googlePlacesApiKey?: string;
 
-  constructor() {}
+  constructor(
+    public apiKeyService: ApiKeyService,
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+     this.apiKeyService.getGooglePlacesApiKey().subscribe({
+      next: (data: any) => {
+        this.googlePlacesApiKey = data.googlePlacesApiKey;
+      },
+      error: (err: any) => {
+        console.log("No ha recibido la KEY de Google Places API.")
+      }
+    });
+  }
 
   openInfoWindow(index: number, markerData: any) {
     this.activeMarker = markerData;
+    this.activePhotoIndex = 0; // Reset al abrir
+    this.selectedPlaceImages = markerData?.place?.photos || [];
     const marker = this.markerRefs.get(index);
     if (marker) {
       this.infoWindow.open(marker);
     }
   }
 
+  prevPhoto(event: Event) {
+    event.stopPropagation();
+    if (this.activeMarker?.place?.photos?.length) {
+      this.activePhotoIndex =
+        (this.activePhotoIndex - 1 + this.activeMarker.place.photos.length) %
+        this.activeMarker.place.photos.length;
+    }
+  }
+
+  nextPhoto(event: Event) {
+    event.stopPropagation();
+    if (this.activeMarker?.place?.photos?.length) {
+      this.activePhotoIndex =
+        (this.activePhotoIndex + 1) % this.activeMarker.place.photos.length;
+    }
+  }
+
   getPhotoUrl(photo: any): string {
     // Igual que en SearchPlacesComponent, o pásalo como @Input
-    return photo?.name ? `https://places.googleapis.com/v1/${photo.name}/media?maxHeightPx=400&key=TU_API_KEY` : 'assets/no-image.png';
+    return photo?.name ? `https://places.googleapis.com/v1/${photo.name}/media?maxHeightPx=400&key=${this.googlePlacesApiKey}` : 'assets/no-image.png';
   }
 }
